f42328e816e9badb00b6a8d0622a74ce156a4281
2dnerd <prajwalprvce@gmail.com>
2013-12-12 08:15:46 +0530

    first app with app_param auth_user load_user next() using express

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.bodyParser());
+app.use(app.router);
+app.use(express.static(__dirname + "/public"));
+
+var users = [
+	{name:"prajwal"},
+	{name:"tejas"},
+	{name:"abc"}
+];
+
+function authUser(req,res,next){
+	console.log("in authuser");
+	var authDone = false;
+	for(var index=0;index<users.length;index++){
+		if(users[index].name + "OK" == req.userid){
+			authDone = true;
+			console.log("authenticated");
+			break;
+		}
+	}
+
+	if(!authDone){
+		console.log("failed to authenticate");
+	}
+
+	next();
+}
+
+function loadUser(req,res,next){
+	console.log("in loaduser");
+	req.userid = req.userid.substring(0,3) + "     CONTENT";
+	next();
+}
+
+app.param('userId',function(req,res,next,userId){
+	console.log("in app.param");
+	req.userid = userId + "OK";
+	next();
+});
+ 
+
+app.get("/users/:userId",authUser,loadUser,function(req,res,next){
+
+	res.json(req.userid);
+
+});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
